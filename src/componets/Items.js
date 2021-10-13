8639f03f56adfcf660b1d3a3c37a68b243b4e703
RTBlanco <ronnytoribio1@hotmail.com>
2021-10-13 14:18:37 -0400

    rendering all the items

--- src/componets/Items.js
+++ src/componets/Items.js
@@ -2,9 +2,14 @@ import Item from "./Item";
 import './Items.css'
 
 const Items = () => {
+
+  const renderItems = () => {
+    return [1,2,3,4,5,6,7,8,9,10].map((num, i) => <Item item={num} key={i} />)
+  }
+
   return (
     <div id="items-container">
-
+      {renderItems()}
     </div>
   );
 };
